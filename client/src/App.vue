8e52491512f2eeb75dc6e4a6f93d2d34c03eef78
Immanuel Pandiangan <nuelpan@gmail.com>
2020-01-22 16:43:47 +0700

    add vue cli skeleton add firebase config,

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import db from '../config/firebase'
+
+export default {
+
+  name: 'app',
+  data () {
+    return {}
+  },
+  methods: {
+  },
+  created () {
+  }
+}
+
+</script>
+
+<style>
+
+</style>
