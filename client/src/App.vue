d5b4fd78b4c9ba497820c4011021a74dae0e97e8
Immanuel Pandiangan <nuelpan@gmail.com>
2020-01-22 21:21:56 +0700

    add navigation bar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,9 +10,55 @@ export default {
 
   name: 'app',
   data () {
-    return {}
+    return {
+      data: [
+        {
+          title: 'Backlog',
+          color: 'danger',
+          tasks: []
+        },
+        {
+          title: 'Todo',
+          color: 'warning',
+          tasks: []
+        },
+        {
+          title: 'Doing',
+          color: 'info',
+          tasks: []
+        },
+        {
+          title: 'done',
+          color: 'success',
+          tasks: []
+        }
+      ]
+    }
   },
   methods: {
+    getData () {
+      db.collection('kanban').onSnapshot(querySnapshot => {
+        this.dataBacklog.tasks = []
+        this.dataTodo.tasks = []
+        this.dataDoing.tasks = []
+        this.dataDone.tasks = []
+
+        querySnapshot.forEach(doc => {
+          if (doc.data().status === 'backlog') {
+            this.dataBacklog.tasks.push({ id: doc.id, ...doc.data() })
+          }
+          if (doc.data().status === 'todo') {
+            this.dataTodo.tasks.push({ id: doc.id, ...doc.data() })
+          }
+          if (doc.data().status === 'doing') {
+            this.dataDoing.tasks.push({ id: doc.id, ...doc.data() })
+          }
+          if (doc.data().status === 'done') {
+            this.dataDone.tasks.push({ id: doc.id, ...doc.data() })
+          }
+        })
+      })
+    }
   },
   created () {
   }
